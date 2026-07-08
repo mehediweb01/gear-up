286cec3054cd4bae3343540b3f1b14b5fa860118
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2026-07-09 03:30:19 +0600

    error deploy

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/gear", gearRoutes);
 app.use("/api/category", categoryRoutes);
